b560ea248c7c699c877691fd8cd2fb86c418c9e2
LabWork <labetzke@mail.ru>
2021-03-29 17:16:55 +0300

    nice commit

---
+++ www/script.js
@@ -0,0 +1,13 @@
+var composition1 = {
+  adventure:"AlyY",
+  generals:["Vargus", "Silvana", "Boris", "Silvana", "MBI", "MBI", "MBI", "MRS", "Simple", "Simple", "Intendant"],
+  doubleComp: true,
+  file:"AlyY_Double.cms"
+}
+
+var composition2 = {
+  adventure:"Aly_Y";
+}
+
+var json = JSON.stringify(composition1);
+alert(typeof json);
